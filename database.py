56f3ecc7f12514b0482af96fb4eae834fc0fb4de
Salavatpy <karimov.slvt@gmail.com>
2022-07-08 17:58:42 +0600

    PosgreSQL added 3

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 database_url = os.getenv('DATABASE_URL')
 
 if database_url.startswith('postgres://'):
-    database_url.replace('postgres://', 'postgresql://')
+    database_url.replace('postgres://', 'postgresql+psycopg2://')
 
 engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
