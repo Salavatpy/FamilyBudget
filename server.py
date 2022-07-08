1d248d64110706fd7ba26b6c076ce6190fcc97a0
Salavatpy <karimov.slvt@gmail.com>
2022-07-08 12:31:08 +0600

    deployready

--- server.py
+++ server.py
@@ -2,21 +2,39 @@
 
 import logging
 
+from aiogram.utils.executor import start_webhook
+
 import handlers
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 from database import create_db
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from states import States
 from aiogram.dispatcher import FSMContext
 from keyboard import kb_menu, creating_categories_menu, kb_exit
+import os
 
-logging.basicConfig(level=logging.INFO)
-
-API_TOKEN = "5463394888:AAEEZ6p8c69e-kORiwEQksQyHFufW98QTsU"
-
-bot = Bot(token=API_TOKEN)
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -177,5 +195,14 @@ async def add_category_2(message: types.Message, state: FSMContext):
 
 if __name__ == '__main__':
     create_db()
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
 
